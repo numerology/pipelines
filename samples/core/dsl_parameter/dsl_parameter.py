9aca740fe1b10d8075c8ab012063efd621806f84
numerology <jxzheng@google.com>
2019-11-14 11:04:55 -0800

    update

--- samples/core/dsl_parameter/dsl_parameter.py
+++ samples/core/dsl_parameter/dsl_parameter.py
@@ -30,6 +30,7 @@ from kfp import dsl
 from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
 from tfx.orchestration import pipeline
 from tfx.orchestration.experimental.runtime_parameter import runtime_string_parameter
+from tfx.orchestration import data_types
 from tfx.orchestration.kubeflow import kubeflow_dag_runner
 from tfx.utils.dsl_utils import csv_input
 from tfx.proto import example_gen_pb2
@@ -47,8 +48,8 @@ _data_root_param = dsl.PipelineParam(
     value='gs://ml-pipeline-playground/tfx_taxi_simple/data')
 
 # Name of the output split from ExampleGen. Specified as a RuntimeParameter.
-_example_split_name = runtime_string_parameter.RuntimeStringParameter(
-    name='split-name', default='train'
+_example_split_name = data_types.RuntimeParameter(
+    name='split-name', default='train', ptype=str
 )
 
 
@@ -67,20 +68,17 @@ def _create_one_step_pipeline(
   Returns:
     A logical TFX pipeline.Pipeline object.
   """
-  example_split_name = runtime_string_parameter.RuntimeStringParameter(
-      name='split-name', default='train'
-  )
+
   examples = csv_input(str(_data_root_param))
   example_gen = CsvExampleGen(
       input=examples,
-      output_config=example_gen_pb2.Output(
-          split_config=example_gen_pb2.SplitConfig(
-              splits=[
-                  example_gen_pb2.SplitConfig.
-                  Split(name=example_split_name, hash_buckets=10)
+      output_config={
+          'splitConfig': {
+              'splits': [
+                {'name': _example_split_name, 'hashBuckets': 10}
               ]
-          )
-      )
+          }
+      }
   )
   return pipeline.Pipeline(
       pipeline_name=pipeline_name,
