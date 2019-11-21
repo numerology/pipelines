1b29d23aa109685023c10c1f353d4f0f5d96b2c7
numerology <jxzheng@google.com>
2019-11-21 14:01:21 -0800

    update poc

--- samples/core/dsl_parameter/dsl_parameter.py
+++ samples/core/dsl_parameter/dsl_parameter.py
@@ -27,11 +27,22 @@ from typing import Optional, Text
 import kfp
 from kfp import dsl
 
-from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
-from tfx.orchestration import pipeline
 from tfx.orchestration import data_types
+from tfx.components.evaluator.component import Evaluator
+from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
+from tfx.components.example_validator.component import ExampleValidator
+from tfx.components.model_validator.component import ModelValidator
+from tfx.components.pusher.component import Pusher
+from tfx.components.schema_gen.component import SchemaGen
+from tfx.components.statistics_gen.component import StatisticsGen
+from tfx.components.trainer.component import Trainer
+from tfx.components.transform.component import Transform
+from tfx.orchestration import pipeline
 from tfx.orchestration.kubeflow import kubeflow_dag_runner
+from tfx.proto import evaluator_pb2
 from tfx.utils.dsl_utils import csv_input
+from tfx.proto import pusher_pb2
+from tfx.proto import trainer_pb2
 
 # Path of pipeline root, should be a GCS path.
 pipeline_root = os.path.join(
@@ -41,21 +52,38 @@ pipeline_root = os.path.join(
 # Path to the CSV data file, under which their should be a data.csv file.
 # Note: this is still digested as raw PipelineParam b/c parameterization of
 # ExternalArtifact attributes has not been implemented yet in TFX.
-_data_root_param = dsl.PipelineParam(
+_data_root_param = data_types.RuntimeParameter(
     name='data-root',
-    value='gs://ml-pipeline-playground/tfx_taxi_simple/data')
-
-# Name of the output split from ExampleGen. Specified as a RuntimeParameter.
-_example_split_name = data_types.RuntimeParameter(
-    name='split-name', default='train', ptype=Text
+    default='gs://ml-pipeline-playground/tfx_taxi_simple/data',
+    ptype=Text,
 )
 
-_example_buckets = data_types.RuntimeParameter(
-    name='buckets', default=10, ptype=int
+# Path to the module file
+_taxi_module_file_param = data_types.RuntimeParameter(
+    name='module-file',
+    default='gs://ml-pipeline-playground/tfx_taxi_simple/modules/taxi_utils.py',
+    ptype=Text,
 )
 
+_train_steps = data_types.RuntimeParameter(
+    name='train-steps',
+    default=10,
+    ptype=int,
+)
 
-def _create_one_step_pipeline(
+_eval_args = data_types.RuntimeParameter(
+    name='eval-steps',
+    default=5,
+    ptype=int,
+)
+
+_slicing_column = data_types.RuntimeParameter(
+    name='slicing-column',
+    default='trip_start_hour',
+    ptype=Text,
+)
+
+def _create_parameterized_pipeline(
     pipeline_name: Text,
     pipeline_root: Text,
     enable_cache: Optional[bool] = True
@@ -73,19 +101,57 @@ def _create_one_step_pipeline(
 
   examples = csv_input(str(_data_root_param))
   example_gen = CsvExampleGen(
-      input=examples,
-      output_config={
-          'splitConfig': {
-              'splits': [
-                {'name': _example_split_name, 'hashBuckets': _example_buckets}
-              ]
-          }
-      }
+      input=examples
   )
+
+  statistics_gen = StatisticsGen(input_data=example_gen.outputs.examples)
+  infer_schema = SchemaGen(
+      stats=statistics_gen.outputs.output, infer_feature_shape=False)
+  validate_stats = ExampleValidator(
+      stats=statistics_gen.outputs.output, schema=infer_schema.outputs.output)
+  transform = Transform(
+      input_data=example_gen.outputs.examples,
+      schema=infer_schema.outputs.output,
+      module_file=_taxi_module_file_param)
+  trainer = Trainer(
+      module_file=_taxi_module_file_param,
+      transformed_examples=transform.outputs.transformed_examples,
+      schema=infer_schema.outputs.output,
+      transform_output=transform.outputs.transform_output,
+      train_args=dict(num_steps=_train_steps),
+      eval_args=dict(num_steps=_eval_args))
+  model_analyzer = Evaluator(
+      examples=example_gen.outputs.examples,
+      model_exports=trainer.outputs.output,
+      feature_slicing_spec=dict(
+          specs=[
+              dict(column_for_slicing=[_slicing_column])
+          ]
+      ))
+  model_validator = ModelValidator(
+      examples=example_gen.outputs.examples, model=trainer.outputs.output)
+
+  # Hack: ensuring push_destination can be correctly parameterized and interpreted.
+  # pipeline root will be specified as a dsl.PipelineParam with the name
+  # pipeline-root, see:
+  # https://github.com/tensorflow/tfx/blob/1c670e92143c7856f67a866f721b8a9368ede385/tfx/orchestration/kubeflow/kubeflow_dag_runner.py#L226
+  _pipeline_root_param = dsl.PipelineParam(name='pipeline-root')
+  pusher = Pusher(
+      model_export=trainer.outputs.output,
+      model_blessing=model_validator.outputs.blessing,
+      push_destination=pusher_pb2.PushDestination(
+          filesystem=pusher_pb2.PushDestination.Filesystem(
+              base_directory=os.path.join(
+                  str(_pipeline_root_param), 'model_serving'))))
+
+
   return pipeline.Pipeline(
       pipeline_name=pipeline_name,
       pipeline_root=pipeline_root,
-      components=[example_gen],
+      components=[
+        example_gen, statistics_gen, infer_schema, validate_stats, transform,
+        trainer, model_analyzer, model_validator, pusher
+      ],
       enable_cache=enable_cache,
   )
 
@@ -93,7 +159,7 @@ def _create_one_step_pipeline(
 if __name__ == '__main__':
 
   enable_cache = True
-  pipeline = _create_one_step_pipeline(
+  pipeline = _create_parameterized_pipeline(
       'dsl_parameter', pipeline_root, enable_cache=enable_cache
   )
   config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
@@ -102,7 +168,5 @@ if __name__ == '__main__':
       tfx_image='tensorflow/tfx:latest'
   )
   kfp_runner = kubeflow_dag_runner.KubeflowDagRunner(config=config, output_filename='dsl_parameter.yaml')
-  # Make sure kfp_runner recognizes those parameters.
-  kfp_runner._params.extend([_data_root_param])
 
   kfp_runner.run(pipeline)
